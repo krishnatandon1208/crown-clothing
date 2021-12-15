59def50a1298c3138a1997a69e10308b587f2822
Krishna Tandon <krishnatandon.1208@gmail.com>
2021-12-16 00:54:53 +0530

    update menu items and routing for hats/jackets using RR-DOM v6

--- src/App.js
+++ src/App.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
 
-function App() {
+const HatsPage = () => {
+  let navigate = useNavigate();
   return (
     <div>
-      <Homepage />
+      <h1>Hats here</h1>
+      <button onClick={() => navigate('/')}>HOME</button>
     </div>
+  );
+}
+
+const JacketsPage = () => {
+  let navigate = useNavigate();
+  return (
+    <div>
+      <h1>Jackets here</h1>
+      <button onClick={() => navigate('/')}>HOME</button>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <Routes>
+      <Route exact path='/' element={<Homepage />} />
+      <Route exact path='/crown-clothing' element={<Homepage />} />
+      <Route path='/shop/hats' element={<HatsPage />} />
+      <Route path='/shop/jackets' element={<JacketsPage />} />
+    </Routes>
   );
 }
 
--- src/components/directory/directory.component.js
+++ src/components/directory/directory.component.js
@@ -47,8 +47,12 @@ export default class Directory extends React.Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({ title, imageUrl, id, size }) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
+                    /* this.state.sections.map(({ title, imageUrl, id, size, linkUrl }) => (
+                        <MenuItem key={id} linkUrl={linkUrl} title={title} imageUrl={imageUrl} size={size} />
+                    )) */
+
+                    this.state.sections.map(({ id, ...otherSectionLink }) => (
+                        <MenuItem key={id} {...otherSectionLink} />
                     ))
                 }
             </div>
